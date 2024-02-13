0d0458ac1baded5693415f28833030de2083e2eb
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-02-13 08:37:11 +0600

    add login

---
+++ src/features/authentication/LoginForm.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import Button from '../../ui/Button'
+import Form from '../../ui/Form'
+import Input from '../../ui/Input'
+import FormRowVertical from '../../ui/FormRowVertical'
+import { useLogin } from './useLogin'
+import SpinnerMini from '../../ui/SpinnerMini'
+
+function LoginForm() {
+	const [email, setEmail] = useState('bogatyrev@example.com')
+	const [password, setPassword] = useState('password')
+
+	const { login, isLogging } = useLogin()
+
+	function handleSubmit(e) {
+		e.preventDefault()
+
+		if (!email || !password) return
+		login({ email, password })
+	}
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<FormRowVertical label='Email address'>
+				<Input
+					type='email'
+					id='email'
+					autoComplete='username'
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					disabled={isLogging}
+				/>
+			</FormRowVertical>
+			<FormRowVertical label='Password'>
+				<Input
+					type='password'
+					id='password'
+					autoComplete='current-password'
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					disabled={isLogging}
+				/>
+			</FormRowVertical>
+			<FormRowVertical>
+				<Button size='large' disabled={isLogging}>
+					{isLogging ? <SpinnerMini /> : 'Login'}
+				</Button>
+			</FormRowVertical>
+		</Form>
+	)
+}
+
+export default LoginForm
--- src/ui/Heading.tsx
+++ src/ui/Heading.tsx
@@ -1,25 +1,40 @@
 import styled, { css } from 'styled-components'
 
+interface HeadingProps {
+	type?: 'h1' | 'h2' | 'h3' | 'h4'
+}
+
 const test = css`
 	text-align: center;
 `
 
-const Heading = styled.h1`
+const Heading = styled.h1<HeadingProps>`
 	${props =>
 		props.type === 'h1' &&
-		`font-size: 3rem;
-	font-weight: 600;`}
+		`
+    font-size: 3rem;
+    font-weight: 600;
+  `}
 	${props =>
 		props.type === 'h2' &&
-		`font-size: 2rem;
-	font-weight: 600;`}
+		`
+    font-size: 2rem;
+    font-weight: 600;
+  `}
   ${props =>
 		props.type === 'h3' &&
-		`font-size: 2rem;
-	font-weight: 500;`}
-
-	line-height: 1.4;
-
+		`
+    font-size: 2rem;
+    font-weight: 500;
+  `}
+  ${props =>
+		props.type === 'h4' &&
+		`
+    font-size: 3rem;
+    text-align: center;
+    font-weight: 600;
+  `}
+  line-height: 1.4;
 	${test}
 `
 
