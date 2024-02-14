6cea8316d0d2a7497abdcb6267cfc1eed008e44c
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-02-14 06:28:25 +0600

    fix bug

--- src/features/authentication/useLogin.ts
+++ src/features/authentication/useLogin.ts
@@ -1,15 +1,17 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { login as loginApi } from '../../services/apiAuth'
 import { useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast'
 
 export const useLogin = () => {
+	const queryClient = useQueryClient()
 	const navigate = useNavigate()
 
 	const { mutate: login, isPending: isLogging } = useMutation({
 		mutationFn: ({ email, password }: { email: string; password: string }) =>
 			loginApi({ email, password }),
-		onSuccess: () => {
+		onSuccess: user => {
+			queryClient.setQueryData(['user'], user.user)
 			navigate('/dashboard', { replace: true })
 		},
 		onError: err => {
