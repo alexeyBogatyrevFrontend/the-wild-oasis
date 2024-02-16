6dc694f426f8a0cb0918f3866233f93dfa1191b8
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2024-02-16 07:56:47 +0600

    add stays for current day

--- src/features/check-in-out/CheckoutButton.tsx
+++ src/features/check-in-out/CheckoutButton.tsx
@@ -1,11 +1,24 @@
-import Button from "../../ui/Button";
+import { FC } from 'react'
+import Button from '../../ui/Button'
+import { useCheckout } from './useCheckout'
 
-function CheckoutButton({ bookingId }) {
-  return (
-    <Button variation="primary" size="small">
-      Check out
-    </Button>
-  );
+type CheckoutButtonType = {
+	bookingId: number
 }
 
-export default CheckoutButton;
+const CheckoutButton: FC<CheckoutButtonType> = ({ bookingId }) => {
+	const { checkout, isCheckingOut } = useCheckout()
+
+	return (
+		<Button
+			variant='primary'
+			size='small'
+			onClick={() => checkout(bookingId)}
+			disabled={isCheckingOut}
+		>
+			Check out
+		</Button>
+	)
+}
+
+export default CheckoutButton
